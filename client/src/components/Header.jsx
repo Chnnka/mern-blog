4401b9e31d04a4dcd966ce736a0582a322d14bd5
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-01-21 19:14:39 +0530

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Button, Navbar } from 'flowbite-react';
+import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
   return (
     <Navbar className="border-b-2">
       <Link
         to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        StoryStore
+        <span className="px-2 py-1 bg-gray-300 rounded-lg">Story</span>
+        Store
       </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray">
+        <AiOutlineSearch />
+      </Button>
+      <div>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+      </div>
     </Navbar>
   );
 }
