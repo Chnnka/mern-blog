fae3288f6bb11a2134a5625611eb71023bb99bb6
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-01-26 18:56:02 +0530

    header errors fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,52 +1,70 @@
-import React from 'react';
-import { Button, Navbar, NavbarLink, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gray-300 rounded-lg">Story</span>
+        <span className="px-2 py-1 bg-gradient-to-r from-gray-500 via-black-500 to-slate-500 rounded-lg text-white">
+          Story
+        </span>
         Store
       </Link>
       <form>
         <TextInput
           type="text"
-          placeholder="search..."
+          placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray">
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
-        </Button>
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+        ></Button>
+        <Dropdown inline>
+          <Dropdown.Header>
+            <span className="block text-sm">@</span>
+            <span className="block text-sm font-medium truncate"></span>
+          </Dropdown.Header>
+          <Link to="/dashboard">
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+
         <Link to="/sign-in">
-          <Button color="gray" pill>
+          <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
-        <Navbar.toggle />
-        <Navbar.Collapse>
-          <NavbarLink>
-            <Link to="/">Home</Link>
-          </NavbarLink>
-          <NavbarLink>
-            <Link to="/about">About</Link>
-          </NavbarLink>
-          <NavbarLink>
-            <Link to="/projects">Projects</Link>
-          </NavbarLink>
-        </Navbar.Collapse>
+
+        <Navbar.Toggle />
       </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
