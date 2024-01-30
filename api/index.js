4f9bb9138d16a907d9a0f2739b5140fab2e7a088
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-01-30 09:13:45 +0530

    error handler

--- api/index.js
+++ api/index.js
@@ -23,4 +23,14 @@ app.listen(
 );
 
 app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal server error';
+  res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  });
+});
